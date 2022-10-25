afd48fd5c81ec268809b0f0ead8c6d1777fb862a
Radwan albahrani <radwan158@gmail.com>
2022-10-25 11:00:29 +0300

    User Profile Authentication created

---
+++ src/Profile.java
@@ -0,0 +1,113 @@
+import java.sql.Date;
+
+public class Profile
+{
+    String name, nationality, bloodType, field, additionalField, email, phoneNumber;
+    int age;
+    char gender;
+    Date birthDate;
+
+    // Setters
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality)
+    {
+        this.nationality = nationality;
+    }
+
+    public void setBloodType(String bloodType)
+    {
+        this.bloodType = bloodType;
+    }
+
+    public void setField(String field)
+    {
+        this.field = field;
+    }
+
+    public void setAdditionalField(String additionalField)
+    {
+        this.additionalField = additionalField;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public void setGender(char gender)
+    {
+        this.gender = gender;
+    }
+
+    public void setBirthDate(Date birthDate)
+    {
+        this.birthDate = birthDate;
+    }
+
+    // Getters
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getNationality()
+    {
+        return nationality;
+    }
+
+    public String getBloodType()
+    {
+        return bloodType;
+    }
+
+    public String getField()
+    {
+        return field;
+    }
+
+    public String getAdditionalField()
+    {
+        return additionalField;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public char getGender()
+    {
+        return gender;
+    }
+
+    public Date getBirthDate()
+    {
+        return birthDate;
+    }
+
+    
+}
---
+++ src/User.java
@@ -0,0 +1,72 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User
+{
+    public static int numberOfUsers = 0;
+    int authorityLevel, age;
+    Date creationDate, expirationDate;
+    Authentication auth;
+    Profile profile;
+
+    public Date getCreationDate()
+    {
+        return creationDate;
+    }
+
+    public Date getExpirationDate()
+    {
+        return expirationDate;
+    }
+
+    public void register(int authorityLevel, String username, String password, String name, String nationality, String bloodType, String field,
+            String additionalField, String email, String phoneNumber, Date birthDate, char gender) // user login
+    {
+        // Setting up Authentication
+        auth.setUsername(username);
+        auth.setPassword(password);
+        auth.setUserID(numberOfUsers++);
+        auth.setAuthorityLevel(authorityLevel);
+
+        // Setting up Profile
+        profile.setName(name);
+        profile.setPhoneNumber(phoneNumber);
+        profile.setEmail(email);
+        profile.setField(field);
+        profile.setAdditionalField(additionalField);
+        profile.setAge(age);
+        profile.setBirthDate(birthDate);
+        profile.setBloodType(bloodType);
+        profile.setNationality(nationality);
+        profile.setGender(gender);
+
+    }
+
+    public List<String> viewProfile() // allow to view profile
+    {
+        // Setting up Profile
+        List<String> viewProfile = new ArrayList<String>();
+        viewProfile.add(profile.getName());
+        viewProfile.add(profile.getPhoneNumber());
+        viewProfile.add(profile.getEmail());
+        viewProfile.add(profile.getField());
+        viewProfile.add(profile.getAdditionalField());
+        viewProfile.add(profile.getBloodType());
+        viewProfile.add(profile.getNationality());
+        // viewProfile.add(profile.getAge());
+        // viewProfile.add(profile.getBirthDate());
+        // viewProfile.add(profile.getGender());
+
+        return viewProfile;
+    }
+
+    {
+
+    }
+
+    public void requestChange() // request change from admin
+    {
+
+    }
+}
