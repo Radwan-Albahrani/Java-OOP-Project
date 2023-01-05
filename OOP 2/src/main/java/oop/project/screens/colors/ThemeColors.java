844574f73eea304650c126f80f42f388fb853bdd
Yosef Ahmed <Yosef13103@gmail.com>
2023-01-05 19:40:15 +0300

    ThemeColors

--- OOP 2/src/main/java/oop/project/screens/colors/ThemeColors.java
+++ OOP 2/src/main/java/oop/project/screens/colors/ThemeColors.java
@@ -4,10 +4,22 @@ import java.awt.Color;
 
 public class ThemeColors
 {
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color WHITE = new Color(255, 255, 255);
     public static final Color POWDER_BLUE = new Color(176, 224, 230);
     public static final Color DARK_POWDER_BLUE = new Color(135, 206, 235);
     public static final Color MEDIUM_SEA_GREEN = new Color(60, 179, 113);
     public static final Color DARK_SEA_GREEN = new Color(143, 188, 143);
     public static final Color KHAKI = new Color(240, 230, 140);
     public static final Color LIGHT_KHAKI = new Color(255, 245, 155);
+    
+    public static final Color GUNMETAL = new Color(43, 48, 58);
+    public static final Color MIDDLE_BLUE_COLOR = new Color(128, 128, 128);
+    public static final Color LAVENDER_BLUSH = new Color(255, 240, 245);
+    public static final Color CINNABAR = new Color(227, 66, 52);
+    public static final Color ORANGE_PEEL = new Color(255, 159, 67);
+    public static final Color TIFFANY_BLUE = new Color(10, 186, 181);
+    public static final Color ROSE_MADDER = new Color(211, 211, 211);
+    public static final Color BLURPLE = new Color(78, 93, 148);
+
 }
