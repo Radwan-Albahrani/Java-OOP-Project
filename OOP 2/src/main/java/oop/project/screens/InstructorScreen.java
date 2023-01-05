f688923042950169e905a26683bfdce1bda00ba7
Radwan-Albahrani <radwan158@gmail.com>
2023-01-05 21:01:34 +0300

    NavBar

--- OOP 2/src/main/java/oop/project/screens/InstructorScreen.java
+++ OOP 2/src/main/java/oop/project/screens/InstructorScreen.java
@@ -31,40 +31,39 @@ public class InstructorScreen extends JFrame
     KGradientPanel buttonPanel = new ThemedPanel(); // Panel for the buttons
     KGradientPanel mainPanel = new ThemedPanelStudent(); // Panel for the main screen
     KGradientPanel announcementPanel = new ThemedPanelStudent(); // Panel for the announcement screen
-        KGradientPanel viewStudentsPanel = new ThemedPanelStudent(); // Panel for the view students screen
-        KGradientPanel editGradesPanel = new ThemedPanelStudent(); // Panel for the edit grades screen
+    KGradientPanel viewStudentsPanel = new ThemedPanelStudent(); // Panel for the view students screen
+    KGradientPanel editGradesPanel = new ThemedPanelStudent(); // Panel for the edit grades screen
     KGradientPanel profilePanel = new ThemedPanelStudent(); // Panel for the profile screen
 
-    //Button Setup
+    // Button Setup
     KButton MenuButton = new CustomButton("Main Menu"); // Button to go back to the main menu
     KButton AnnouncementButton = new CustomButton("Add Announcement"); // Button to add an announcement
     KButton StudentButton = new CustomButton("View Students"); // Button to view the students
     KButton StudentViewButton = new CustomButton("View Students"); // Button to view the students
-    KButton editGradeButton = new CustomButton("Edit Grades");  // Button to edit grades
-    KButton ProfileButton = new CustomButton("View Profile");   // Button to view profile
-    KButton LogoutButton = new CustomButton("Logout");  // Button to logout
-    
-    // Box Setup
-    JComponent[] mainbuttonComponents = {AnnouncementButton, StudentButton, ProfileButton}; // Components for the Main Menu
-    Box mainButtonBox = AddToBox.addToHorizontalBox(mainbuttonComponents, 3); // Box for the Main Menu
-    JComponent[] studentbuttonComponents = {MenuButton, StudentViewButton, editGradeButton}; // Components for the Student Menu
-    Box studentbuttonBox = AddToBox.addToHorizontalBox(studentbuttonComponents, 3); // Box for the Student Menu
-    
+    KButton editGradeButton = new CustomButton("Edit Grades"); // Button to edit grades
+    KButton ProfileButton = new CustomButton("View Profile"); // Button to view profile
+    KButton LogoutButton = new CustomButton("Logout"); // Button to logout
 
-    public InstructorScreen()   
+    // Box Setup
+    JComponent[] mainButtonComponents = {AnnouncementButton, StudentButton, ProfileButton}; // Components for the Main Menu
+    Box mainButtonBox = AddToBox.addToHorizontalBox(mainButtonComponents, 3); // Box for the Main Menu
+    JComponent[] studentButtonComponents = {MenuButton, StudentViewButton, editGradeButton}; // Components for the Student Menu
+    Box studentButtonBox = AddToBox.addToHorizontalBox(studentButtonComponents, 3); // Box for the Student Menu
+
+    public InstructorScreen()
     {
         // Frame Setup
         FrameConfig.set(this, "Instructor"); // Creating the frame
-        
+
         // Left Button Panel Setup (Will be shown by default)
         AnnouncementButton.setPreferredSize(new Dimension(250, 50));
         buttonPanel.setLayout(new BorderLayout());
         buttonPanel.setPreferredSize(new Dimension(250, 0));
 
         // Main Panel Setup (Will be shown by default)
-        JLabel welcomeLabel = new JLabel("Welcome, Instructor"); //TODO: Change to actual name
-        //TODO: if the instructor is not in a course, show a message saying they are not in a course
-        //     and give them the option to join a course
+        JLabel welcomeLabel = new JLabel("Welcome, Instructor"); // TODO: Change to actual name
+        // TODO: if the instructor is not in a course, show a message saying they are not in a course
+        // and give them the option to join a course
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 30));
         mainPanel.add(welcomeLabel);
         // mainButtonComponents for the Main Menu
@@ -72,14 +71,14 @@ public class InstructorScreen extends JFrame
         buttonPanel.add(LogoutButton, BorderLayout.SOUTH);
 
         // Announcement Panel Setup (Will replace Main Panel when Announcement Button is clicked)
-        JLabel announcementLabel = new JLabel("Please add an annoucement");
+        JLabel announcementLabel = new JLabel("Please add an announcement");
         KButton sendButton = new CustomButton("Send");
         sendButton.setPreferredSize(new Dimension(100, 50));
         JTextField announcementTextField = new JTextField();
         announcementTextField.setPreferredSize(new Dimension(200, 50));
         announcementLabel.setFont(new Font("Arial", Font.BOLD, 30));
         announcementPanel.add(announcementLabel);
-        announcementPanel.add(announcementTextField); //TODO: Add a textfield to add the announcement
+        announcementPanel.add(announcementTextField); // TODO: Add a textfield to add the announcement
         announcementPanel.add(sendButton);
 
         // Student Panel Setup (Will replace Main Panel when Student Button is clicked)
@@ -97,11 +96,12 @@ public class InstructorScreen extends JFrame
         profilePanel.add(profileLabel);
 
         // Adding the panels to the frame
-        add(buttonPanel, BorderLayout.WEST); //add the button panel to the left 
-        add(mainPanel, BorderLayout.CENTER); //add the main panel to the center
+        JPanel navBar = new NavBar(this);
+        add(navBar, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.WEST); // add the button panel to the left
+        add(mainPanel, BorderLayout.CENTER); // add the main panel to the center
 
         setVisible(true);
-
 
         // Action Listeners
         ButtonHandler handler = new ButtonHandler();
@@ -112,8 +112,8 @@ public class InstructorScreen extends JFrame
         LogoutButton.addActionListener(handler);
     }
 
-//TODO: use buttonPanel.remove() to remove the current panel and add the new one with an if statement assuming the button is clicked
-    
+    // TODO: use buttonPanel.remove() to remove the current panel and add the new one with an if statement assuming the button is clicked
+
     public class ButtonHandler implements ActionListener
     {
         @Override
@@ -122,14 +122,15 @@ public class InstructorScreen extends JFrame
             String buttonClicked = e.getActionCommand();
             if (buttonClicked.equals("Main Menu"))
             {
+                remove(announcementPanel);
                 remove(viewStudentsPanel);
                 remove(editGradesPanel);
                 add(mainPanel, BorderLayout.CENTER);
-                buttonPanel.remove(studentbuttonBox);
+                buttonPanel.remove(studentButtonBox);
                 buttonPanel.add(mainButtonBox, BorderLayout.NORTH);
             }
 
-            if (buttonClicked.equals("Add Announcement"))
+            else if (buttonClicked.equals("Add Announcement"))
             {
                 remove(mainPanel);
                 remove(profilePanel);
@@ -141,7 +142,7 @@ public class InstructorScreen extends JFrame
             else if (buttonClicked.equals("View Students"))
             {
                 buttonPanel.remove(mainButtonBox);
-                buttonPanel.add(studentbuttonBox, BorderLayout.NORTH);
+                buttonPanel.add(studentButtonBox, BorderLayout.NORTH);
 
                 remove(mainPanel);
                 remove(announcementPanel);
@@ -177,5 +178,5 @@ public class InstructorScreen extends JFrame
             buttonPanel.revalidate();
             buttonPanel.repaint();
         }
-    }     
+    }
 }
