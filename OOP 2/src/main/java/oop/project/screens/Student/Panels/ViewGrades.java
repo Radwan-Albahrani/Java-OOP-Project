394ec01730147eeecf357e8f669c06b33eba4f6c
safwanx <107620033+safwanx@users.noreply.github.com>
2023-01-06 20:33:19 +0300

    changing student Screen panel

---
+++ OOP 2/src/main/java/oop/project/screens/Student/Panels/ViewGrades.java
@@ -0,0 +1,30 @@
+package oop.project.screens.Student.Panels;
+
+import oop.project.screens.components.BlueButton;
+import oop.project.screens.components.ThemedPanel;
+import oop.project.screens.hooks.AddToBox;
+
+import java.awt.*;
+import javax.swing.*;
+import com.k33ptoo.components.*;
+
+public class ViewGrades extends ThemedPanel
+{
+    public ViewGrades(int Width, int Height)
+    {
+        JLabel ViewGradesLabel = new JLabel("View Grades");
+        ViewGradesLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        JTextArea ViewGradesTextArea = new JTextArea();
+        ViewGradesTextArea.setPreferredSize(new Dimension(1000, 200));
+        ViewGradesTextArea.setLineWrap(true);
+
+        KButton ViewGradesButton = new BlueButton(" View Grades");
+        ViewGradesButton.setPreferredSize(new Dimension(150, 50));
+
+        JComponent[] ViewGradesComponents = {ViewGradesLabel, ViewGradesTextArea, ViewGradesButton};
+        Box ViewGradesBox = AddToBox.addToVerticalBox(ViewGradesComponents, 1);
+
+        this.add(ViewGradesBox);
+    }
+}
--- OOP 2/src/main/java/oop/project/screens/Student/Student.java
+++ OOP 2/src/main/java/oop/project/screens/Student/Student.java
@@ -17,13 +17,21 @@ public class Student extends JFrame
     {
         FrameConfig.set(this, "Student");
 
-        KGradientPanel mainPanel = new StudentMain(getWidth(), getHeight());
+        KGradientPanel StudentMain = new StudentMain(getWidth(), getHeight());
         KGradientPanel registerClass = new registerClass(getWidth(), getHeight());
         KGradientPanel ViewAlerts = new ViewAlerts(getWidth(), getHeight());
         KGradientPanel DropClass = new DropClass(getWidth(), getHeight());
-        
+        KGradientPanel ViewGrades = new ViewGrades(getWidth(), getHeight());
 
+        KGradientPanel[] studentPanels = {StudentMain, registerClass, ViewAlerts,DropClass,ViewGrades};
 
+        KButton[] studentButtons = {
+            new StudentMainButton("Student Main", studentPanels),
+            new registerClassButton("Register Class", studentPanels),
+            new ViewAlertsButton("View Alerts", studentPanels),
+            new DropClassButton("Drop Class", studentPanels),
+            new ViewGradesButton("View Grades", studentPanels)
+        };
 
     }
 }
