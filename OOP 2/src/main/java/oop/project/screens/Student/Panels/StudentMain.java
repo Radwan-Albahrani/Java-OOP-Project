925a17a3639061c050f32f3426f5ee0bc0a7d149
safwanx <107620033+safwanx@users.noreply.github.com>
2023-01-06 20:27:16 +0300

    added panels

---
+++ OOP 2/src/main/java/oop/project/screens/Student/Panels/StudentMain.java
@@ -0,0 +1,9 @@
+package oop.project.screens.Student.Panels;
+
+import oop.project.screens.components.ThemedPanel;
+
+public class StudentMain extends ThemedPanel
+{
+    public StudentMain(int Width, int Height)
+    {}
+}
--- OOP 2/src/main/java/oop/project/screens/Student/Student.java
+++ OOP 2/src/main/java/oop/project/screens/Student/Student.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import com.k33ptoo.components.*;
 
 import oop.project.App;
+import oop.project.screens.Student.Panels.*;
 import oop.project.screens.components.*;
 import oop.project.screens.hooks.*;
 import oop.project.handlers.*;
@@ -14,38 +15,15 @@ public class Student extends JFrame
 {
     public Student()
     {
-        super("Student");
+        FrameConfig.set(this, "Student");
 
-        //Frame setup
-        FrameConfig.set(this, "Student Screen");
+        KGradientPanel mainPanel = new StudentMain(getWidth(), getHeight());
+        KGradientPanel registerClass = new registerClass(getWidth(), getHeight());
+        KGradientPanel ViewAlerts = new ViewAlerts(getWidth(), getHeight());
+        KGradientPanel DropClass = new DropClass(getWidth(), getHeight());
+        
 
-        //Background setup
-        FrameConfig.setBackground(this, "StudentScreen/background.png");
 
-        //Student Panel setup
-        KGradientPanel studentPanel = new ThemedPanel();
-        studentPanel.setPreferredSize(new Dimension((int) (getWidth() / 2), (int) (getHeight() / 1.4)));
-        studentPanel.setSize(getPreferredSize());
-        studentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 200, 10));
 
-        //Student Label setup
-        JLabel studentLabel = new JLabel("Student");
-        studentLabel.setForeground(Color.WHITE);
-        studentLabel.setFont(new Font("Arial", Font.BOLD, 30));
-        studentLabel.setHorizontalAlignment(JLabel.CENTER);
-        studentLabel.setHorizontalTextPosition(JLabel.CENTER);
-
-        //Border Layout
-        BorderLayout borderLayout = new BorderLayout();
-        setLayout(borderLayout);
-
-        //Register Class Button setup
-        KButton registerClass = new BlueButton("Register Class");
-
-        //Drop Class Button setup
-        KButton dropClass = new BlueButton("Drop Class");
-
-        //View Alerts Button setup
-        KButton viewAlerts = new BlueButton("View Alerts");
     }
 }
