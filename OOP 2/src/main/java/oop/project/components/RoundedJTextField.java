8efb749ca4d7d61aa41c9e268f5016b1a420fc3e
Radwan-Albahrani <radwan158@gmail.com>
2023-01-13 17:40:34 +0300

    Rounded JTextField

---
+++ OOP 2/src/main/java/oop/project/components/RoundedJTextField.java
@@ -0,0 +1,38 @@
+package oop.project.components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class RoundedJTextField extends JTextField
+{
+    private Shape shape;
+
+    public RoundedJTextField(int size)
+    {
+        super(size);
+        setOpaque(false); // As suggested by @AVD in comment.
+    }
+
+    protected void paintComponent(Graphics g)
+    {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+        super.paintComponent(g);
+    }
+
+    protected void paintBorder(Graphics g)
+    {
+        g.setColor(getForeground());
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+    }
+
+    public boolean contains(int x, int y)
+    {
+        if (shape == null || !shape.getBounds().equals(getBounds()))
+        {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+        }
+        return shape.contains(x, y);
+    }
+}
