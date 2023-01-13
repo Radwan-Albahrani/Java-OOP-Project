60f8ddaa9aa5b33fb3397b4ee4c0d70f84682fca
safwanx <107620033+safwanx@users.noreply.github.com>
2023-01-13 13:58:03 +0300

    send alers

---
+++ OOP 2/src/main/java/oop/project/screens/StudentScreen/Panels/SendAlerts.java
@@ -0,0 +1,26 @@
+package oop.project.screens.StudentScreen.Panels;
+
+import oop.project.components.BlueButton;
+import oop.project.components.TransparentPanel;
+import oop.project.hooks.AddToBox;
+
+import java.awt.*;
+import javax.swing.*;
+import com.k33ptoo.components.*;
+
+public class SendAlerts extends TransparentPanel
+{
+    public SendAlerts(int Width, int Height)
+    {
+        JLabel viewAlertsLabel = new JLabel("Alerts");
+        viewAlertsLabel.setFont(new Font("Arial", Font.BOLD, 30));
+
+        KButton SendAlertsButton = new BlueButton(" Send Alerts");
+        SendAlertsButton.setPreferredSize(new Dimension(150, 50));
+
+        JComponent[] SendAlertsComponents = {viewAlertsLabel, SendAlertsButton};
+        Box SendAlertsBox = AddToBox.addToVerticalBox(SendAlertsComponents, 1);
+
+        this.add(SendAlertsBox);
+    }
+}
