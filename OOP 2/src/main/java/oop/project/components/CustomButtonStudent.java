91710cf6a16c96310a6b24fc687358da5dcb841f
Radwan-Albahrani <radwan158@gmail.com>
2023-01-13 17:59:31 +0300

    Rounded

--- OOP 2/src/main/java/oop/project/components/CustomButtonStudent.java
+++ OOP 2/src/main/java/oop/project/components/CustomButtonStudent.java
@@ -1,19 +1,14 @@
 package oop.project.components;
 
-import com.k33ptoo.components.KButton;
-
 import oop.project.colors.ThemeColors;
 
 import java.awt.Font;
 import java.awt.Dimension;
-import javax.swing.border.*;
 
-public class CustomButtonStudent extends KButton
+public class CustomButtonStudent extends RoundedButton
 {
     public CustomButtonStudent(String text)
     {
-        Border border = new LineBorder(ThemeColors.BLACK, 2, true);
-
         this.setText(text);
         this.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
         this.setkStartColor(ThemeColors.GREENISH);
@@ -25,6 +20,6 @@ public class CustomButtonStudent extends KButton
         this.setPreferredSize(new Dimension(400, 50));
         this.setMinimumSize(getPreferredSize());
         this.setMaximumSize(getPreferredSize());
-        this.setBorder(border);
+        this.setkBorderRadius(30);
     }
 }
