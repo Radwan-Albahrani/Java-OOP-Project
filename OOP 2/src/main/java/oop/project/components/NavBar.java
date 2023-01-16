3330289e162d6a75f0d79922bbfb9dbb0c08ab8a
Radwan-Albahrani <radwan158@gmail.com>
2023-01-16 23:42:18 +0300

    Navbar fix

--- OOP 2/src/main/java/oop/project/components/NavBar.java
+++ OOP 2/src/main/java/oop/project/components/NavBar.java
@@ -1,11 +1,9 @@
 package oop.project.components;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 
 import oop.project.colors.ThemeColors;
 import com.k33ptoo.utils.ComponentMoverUtil;
-import com.k33ptoo.utils.ComponentResizerUtil;
 
 import java.awt.*;
 
@@ -36,7 +34,7 @@ public class NavBar extends JPanel
         minimizeButton.setLocation(screenWidth - 106, 0);
         minimizeButton.setBorderPainted(false);
         minimizeButton.setContentAreaFilled(false);
-        minimizeButton.setFocusPainted(false); 
+        minimizeButton.setFocusPainted(false);
         minimizeButton.setOpaque(false);
 
         minimizeButton.addActionListener((ActionEvent e) ->
@@ -54,29 +52,19 @@ public class NavBar extends JPanel
         navBar.add(minimizeButton);
         frame.setLayout(new BorderLayout());
         frame.add(navBar, BorderLayout.NORTH);
-        
-        resizeMoveFrame(frame);
-        
+
+        resizeMoveFrame(frame, navBar);
+
     }
 
-    private static void resizeMoveFrame(JFrame frame) {
-        //Resize and move
-        SwingUtilities.invokeLater(() -> {
-   
-            //Drag around your frame using below
-   
-            ComponentMoverUtil.moveFrame(frame, false, frame.getComponents());
-   
-           // Use below if you want to resize your frame
-            ComponentResizerUtil cr = new ComponentResizerUtil();
-            frame.getRootPane().setBorder(new LineBorder(Color.gray));
-            cr.setSnapSize(new Dimension(5, 5));
-            cr.setDragInsets(new Insets(5, 5, 5, 5));
-          
-            //always register frame to resize
-            cr.registerComponent(frame); 
-   
+    private static void resizeMoveFrame(JFrame frame, JPanel panel)
+    {
+        // Resize and move
+        SwingUtilities.invokeLater(() ->
+        {
+            // Drag around your frame using below
+            ComponentMoverUtil.moveFrame(frame, false, panel);
         });
-    } 
-   
+    }
+
 }
