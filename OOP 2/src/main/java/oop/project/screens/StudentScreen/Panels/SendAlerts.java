339a02666f437ce5078dc6bd1f127c85476258dd
safwanx <107620033+safwanx@users.noreply.github.com>
2023-01-17 20:52:12 +0300

    added textbox for sending alerts

--- OOP 2/src/main/java/oop/project/screens/StudentScreen/Panels/SendAlerts.java
+++ OOP 2/src/main/java/oop/project/screens/StudentScreen/Panels/SendAlerts.java
@@ -1,7 +1,6 @@
 package oop.project.screens.StudentScreen.Panels;
 
-import oop.project.components.BlueButton;
-import oop.project.components.TransparentPanel;
+import oop.project.components.*;
 import oop.project.hooks.AddToBox;
 
 import java.awt.*;
@@ -12,13 +11,16 @@ public class SendAlerts extends TransparentPanel
 {
     public SendAlerts(int Width, int Height)
     {
-        JLabel viewAlertsLabel = new JLabel("Alerts");
-        viewAlertsLabel.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+        JLabel sendAlertsLabel = new JLabel("Alerts");
+        sendAlertsLabel.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
 
-        KButton SendAlertsButton = new BlueButton(" Send Alerts");
+        JTextArea sendAlertsTextArea = new PromptedTextArea("Enter your alert to the admin here.");
+        sendAlertsTextArea.setPreferredSize(new Dimension(1000, Height - 200));
+
+        KButton SendAlertsButton = new BlueButton(" Send ");
         SendAlertsButton.setPreferredSize(new Dimension(150, 50));
 
-        JComponent[] SendAlertsComponents = {viewAlertsLabel, SendAlertsButton};
+        JComponent[] SendAlertsComponents = {sendAlertsLabel, SendAlertsButton};
         Box SendAlertsBox = AddToBox.addToVerticalBox(SendAlertsComponents, 1);
 
         this.add(SendAlertsBox);
