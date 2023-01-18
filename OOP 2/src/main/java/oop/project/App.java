7917fa7f2acd09b5490fe529ef13a29f5a68415e
Radwan-Albahrani <Radwan158@gmail.com>
2023-01-18 17:49:08 +0300

    Hello

--- OOP 2/src/main/java/oop/project/App.java
+++ OOP 2/src/main/java/oop/project/App.java
@@ -8,27 +8,15 @@ import oop.project.screens.StudentScreen.StudentScreen;
 
 public final class App
 {
-    public static String Path = "src/main/resources/images/";
-    // public static String Path = "./classes/images/"; // PATH FOR JAR
+        public static String Path = "src/main/resources/images/";
+        // public static String Path = "./classes/images/"; // PATH FOR JAR
 
-    public static void main(String[] args)
-    {
-<<<<<<< HEAD
-        new LoginScreen();
-<<<<<<< HEAD
-        //new AdminScreen();
-        new InstructorScreen();
-        new StudentScreen();
-        // new LoginScreen();
-        // new AdminScreen();
-=======
-        new AdminScreen();
->>>>>>> 265bad2e34f5cd4c50255702f0424288ca52d854
-        new InstructorScreen();
-=======
-        // new LoginScreen();
-        // new AdminScreen();
-        new StudentScreen();
->>>>>>> efc9853419d181541c6a782234985cdfe12a5405
-    }
+        public static void main(String[] args)
+        {
+                // new AdminScreen();
+                new InstructorScreen();
+                new StudentScreen();
+                // new LoginScreen();
+                // new AdminScreen();
+        }
 }
