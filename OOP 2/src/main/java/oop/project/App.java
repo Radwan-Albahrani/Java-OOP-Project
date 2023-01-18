b3887a92ee8e8f282e048d759527460d77bcd93d
Radwan-Albahrani <radwan158@gmail.com>
2023-01-18 14:49:50 +0300

    Build Images path

--- OOP 2/src/main/java/oop/project/App.java
+++ OOP 2/src/main/java/oop/project/App.java
@@ -1,19 +1,18 @@
 package oop.project;
 
-//import oop.project.screens.LoginScreen.LoginScreen;
-//import oop.project.screens.AdminScreen.AdminScreen;
+import oop.project.screens.LoginScreen.LoginScreen;
+import oop.project.screens.AdminScreen.AdminScreen;
 import oop.project.screens.InstructorScreen.InstructorScreen;
 
 public final class App
 {
     public static String Path = "src/main/resources/images/";
+    // public static String Path = "./classes/images/";
 
     public static void main(String[] args)
     {
-        // new LoginScreen();
-        // new AdminScreen();
+        new LoginScreen();
+        new AdminScreen();
         new InstructorScreen();
-        
-
     }
 }
