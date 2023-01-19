09b14389e2bd9839696574edc8ad2659991abf9c
AzizamerAlmubayedh <116705715+AzizamerAlmubayedh@users.noreply.github.com>
2023-01-19 15:19:09 +0300

    Added

--- OOP 2/src/main/java/oop/project/screens/AdminScreen/Panels/ViewAlerts.java
+++ OOP 2/src/main/java/oop/project/screens/AdminScreen/Panels/ViewAlerts.java
@@ -16,24 +16,69 @@ public class ViewAlerts extends TransparentPanel
 {
     public ViewAlerts(int Width, int Height)
     {
+        String sperator = "\n-----------------------------------------------------------------------------------------------------------\n";
+
         JLabel alertsLabel = new JLabel("Inbox Alerts");
         alertsLabel.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
 
-        JTextArea alertTextArea = new JTextArea();
-        alertTextArea.setPreferredSize(new Dimension(1000, Height - 200));
+        JTextArea alertTextArea = new JTextArea(24, 50);
         alertTextArea.setLineWrap(true);
         alertTextArea.setBackground(ThemeColors.LIGHT_GRAY);
-        alertTextArea.setFont(new Font("Trebuchet MS", Font.PLAIN, 30));
+        alertTextArea.setFont(new Font("Trebuchet MS", Font.PLAIN, 25));
         alertTextArea.setBorder(BorderFactory.createLineBorder(ThemeColors.LIGHT_GREY, 5));
         alertTextArea.setEditable(false);
+
+        JScrollPane scrollBar = new JScrollPane(alertTextArea);
+        scrollBar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollBar.setViewportView(alertTextArea);
+
+
 
         KButton refreshButton = new CustomButton(" Refresh ");
         refreshButton.setPreferredSize(new Dimension(150, 50));
 
-        JComponent[] announcementComponents = {alertsLabel, alertTextArea, refreshButton}; // Components for the Alerts Menu
+        JComponent[] announcementComponents = {alertsLabel, scrollBar, refreshButton}; // Components for the Alerts Menu
         Box announcementBox = AddToBox.addToVerticalBox(announcementComponents, 1);
 
         this.add(announcementBox);
+
+        
+        String dumbyAlert = "Mr.Dalinar:\n Group 8M6 has required me to make a make up quiz due to some network connection issues that took place in 15/1/2023";
+        String dumbyAlert2 = "Ms.jasnah:\n The midterm  grades for CIS321 have been uploaded to the main excell sheet ";
+        String dumbyAlert3 = "Ms.Kittrecken:\n Our CIS 211 team would like to propose an extra, bonus quiz to the current groups due to the low average  ";
+        
+        String dumbyAlert4 = "Ms.Kittrecken:\n Our CIS 211 team would like to propose an extra, bonus quiz to the current groups due to the low average  ";
+        String dumbyAlert5= "Mr.Khalidin:\n Our CIS 211 team would like to propose an extra, bonus quiz to the current groups due to the low average  ";
+        String dumbyAlert6= "Mr.teft:\n Our CIS 211 team would like to propose an extra, bonus quiz to the current groups due to the low average  ";
+
+
+
+
+
+
+        alertTextArea.append(dumbyAlert);
+        alertTextArea.append(sperator);
+        alertTextArea.append(dumbyAlert2);
+        alertTextArea.append(sperator);
+        alertTextArea.append(dumbyAlert3);
+        alertTextArea.append(sperator);
+        alertTextArea.append(dumbyAlert4);
+        alertTextArea.append(sperator);
+        alertTextArea.append(dumbyAlert5);
+        alertTextArea.append(sperator);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+        alertTextArea.append(dumbyAlert6);
+
+
+
+
+
+
 
     }
 }
