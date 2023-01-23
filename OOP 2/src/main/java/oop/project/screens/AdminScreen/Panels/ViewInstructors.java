fc0c6080aaffc012fa7013f0cbb4e248dc87c454
Brentnuto <Yosef13103@gmail.com>
2023-01-23 12:26:45 +0300

    Better table font

--- OOP 2/src/main/java/oop/project/screens/AdminScreen/Panels/ViewInstructors.java
+++ OOP 2/src/main/java/oop/project/screens/AdminScreen/Panels/ViewInstructors.java
@@ -21,51 +21,16 @@ public class ViewInstructors extends TransparentPanel
 
         Box viewInstructorsBox;
 
-        String[] Attributes = {"ID", "First Name", "Last Name", "Department number", "sections he/she teaches",
-        };
-
-        Object[][] Data = {
-                {1, "will", "Smith", 213, "7M4"},
-                {2, "joe", "Doe", 213, "2M2"},
-                {3, "khaladin", "Black", 213, "4M3"},
-                {4, "jasnah", "White", 213, "1M4"},
-                {5, "teft", "Brown", 213, "8M1"},
-                {1, "Dalinar", "Smith", 213, "7M5"},
-                {2, "Black", "Doe", 213, "7M2"},
-                {3, "Logan", "Black", 213, "7M4"},
-                {4, "Fitz", "White", 213, "8M2"},
-                {1, "Chade", "Smith", 213, "1M4"},
-                {2, "Kettricken", "Doe", 213, "8M4"},
-                {3, "Burich", "Black", 213, "7M5"},
-                {4, "Saemon", "White", 213, "8M4"},
-                {1, "Guthwulf", "Smith", 213, "7M1"},
-                {2, "Deornoth", "Doe", 213, "3M4"},
-                {3, "Elias", "Black", 213, "3M1"},
-                {4, "Marya", "White", 213, "7M4"},
-                {1, "Gwenyth", "Smith", 213, "7M1"},
-                {2, "Tiamak", "Doe", 213, "7M2"},
-                {3, "Jamie", "Black", 213, "5M3"},
-                {4, "Jane", "White", 213, "4M5"},
-
-        };
-
-        // JTable table = new JTable(Data, Attributes)
-        // {
-        // public boolean editCellAt(int row, int column, java.util.EventObject e)
-        // {
-        // return false;
-        // }
-        // };
-
         JTable table = new JTable();
         ResultSet resultSet = DatabaseCon.getAllWithTypeRS("Instructor");
         table.setModel(DbUtils.resultSetToTableModel(resultSet));
 
-        table.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        table.setFont(new Font("Trebuchet MS", Font.PLAIN, 15));
         table.setDragEnabled(false);
         table.setRowHeight(40);
         table.setCellSelectionEnabled(false);
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.getTableHeader().setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
         table.getTableHeader().setReorderingAllowed(false);
 
         JScrollPane scrollPaneTable = new JScrollPane(table);
