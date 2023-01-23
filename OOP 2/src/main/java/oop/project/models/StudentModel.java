f7234d53124c6fbf6f4a17c1fb56ecfac204a536
Brentnuto <Yosef13103@gmail.com>
2023-01-23 10:25:38 +0300

    add getStudentsOfInstructorList

---
+++ OOP 2/src/main/java/oop/project/models/StudentModel.java
@@ -0,0 +1,69 @@
+package oop.project.models;
+
+public class StudentModel extends UserModel
+{
+    private String courseID;
+    private String courseName;
+    private double quizGrade;
+    private double midtermGrade;
+    private double finalGrade;
+    private double projectGrade;
+    private double totalGrade;
+
+
+    public String getCourseID() {
+        return courseID;
+    }
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public double getQuizGrade() {
+        return quizGrade;
+    }
+
+    public void setQuizGrade(double quizGrade) {
+        this.quizGrade = quizGrade;
+    }
+
+    public double getMidtermGrade() {
+        return midtermGrade;
+    }
+ 
+    public void setMidtermGrade(double midtermGrade) {
+        this.midtermGrade = midtermGrade;
+    }
+
+    public double getProjectGrade() {
+        return projectGrade;
+    }
+
+    public void setProjectGrade(double projectGrade) {
+        this.projectGrade = projectGrade;
+    }
+
+    public double getFinalGrade() {
+        return finalGrade;
+    }
+
+    public void setFinalGrade(double finalGrade) {
+        this.finalGrade = finalGrade;
+    }
+
+    public double getTotalGrade() {
+        return totalGrade;
+    }
+
+    public void setTotalGrade(double totalGrade) {
+        this.totalGrade = totalGrade;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
+
