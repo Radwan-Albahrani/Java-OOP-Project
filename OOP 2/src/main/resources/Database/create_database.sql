f0e463a09b4cc5a10996bc24459858abbf3df226
Radwan-Albahrani <radwan158@gmail.com>
2023-02-01 19:33:29 +0300

    If exists

--- OOP 2/src/main/resources/Database/create_database.sql
+++ OOP 2/src/main/resources/Database/create_database.sql
@@ -290,7 +290,7 @@ VALUES('2230000005', 'Math-111', 10, 10, 10, 10);
 
 ## ------------------------------------- User -------------------------------------------- 
 
-DROP USER 'JavaApp'@'localhost';
+DROP USER IF EXISTS 'JavaApp'@'localhost';
 CREATE USER 'JavaApp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'JavaApp'@'localhost' WITH GRANT OPTION;
 
